6541cd9435f4b80d540ad7961315abeabbb2f878
Israel-Ferreira <israelfsouza@hotmail.com>
2023-09-10 17:50:19 -0300

    Criando o Endpoint de Cadastro de Filmes

---
+++ FilmesAPI/Controllers/FilmesController.cs
@@ -0,0 +1,26 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class FilmesController : ControllerBase
+{
+
+    private static List<Filme> filmes = new();
+
+
+    [HttpPost]
+    public void CadastrarFilme(Filme filme)
+    {
+        filmes.Add(filme);
+    }
+
+    [HttpGet]
+    public List<Filme> ListarFilmes()
+    {
+        return filmes;
+    }
+}
