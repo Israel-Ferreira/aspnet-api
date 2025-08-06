f411a582ed08f7b0df84f41106572287aa115891
Israel-Ferreira <israelfsouza@hotmail.com>
2025-08-06 01:40:23 -0300

    Adicionando o Endpoint BuscarFilmePorId

--- FilmesAPI/Controllers/FilmesController.cs
+++ FilmesAPI/Controllers/FilmesController.cs
@@ -10,17 +10,34 @@ public class FilmesController : ControllerBase
 {
 
     private static List<Filme> filmes = new();
+    private static int id = 0;
 
 
     [HttpPost]
-    public void CadastrarFilme(Filme filme)
+    public ActionResult CadastrarFilme([FromBody] Filme filme)
     {
+        filme.Id = ++id;
+        Console.WriteLine(filme);
         filmes.Add(filme);
+        return Created();
     }
 
     [HttpGet]
-    public List<Filme> ListarFilmes()
+    public IEnumerable<Filme> ListarFilmes()
     {
         return filmes;
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Filme> BuscarFilme(int id)
+    {
+        var result = filmes.FirstOrDefault(f => f.Id == id);
+
+        if (result == null)
+        {
+            return new NotFoundResult();
+        }
+        
+        return Ok(result);
     }
 }
